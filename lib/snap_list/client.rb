baed0ad5be512dd4e0e566faf9eac7bf01730ecb
Alex <s3ek.mail@gmail.com>
2018-03-14 21:23:54 +0300

    try to figure out why messages are duplicating

--- lib/snap_list/client.rb
+++ lib/snap_list/client.rb
@@ -13,10 +13,10 @@ module SnapList
     private
 
     def event_handler(resp)
-      # load "#{__dir__}/handler/base.rb"
-      # load "#{__dir__}/handler/callback.rb"
-      # load "#{__dir__}/handler/inline.rb"
-      # load "#{__dir__}/handler/message.rb"
+      load "#{__dir__}/handler/base.rb"
+      load "#{__dir__}/handler/callback.rb"
+      load "#{__dir__}/handler/inline.rb"
+      load "#{__dir__}/handler/message.rb"
 
       case resp.message
       when Telegram::Bot::Types::Message
@@ -32,10 +32,26 @@ module SnapList
     end
 
     def bot_listen!
-      Telegram::Bot::Client.run(@token) do |bot|
+      Telegram::Bot::Client.run(@token, logger: Logger.new($stderr)) do |bot|
+        bot.logger.info('Bot has been started')
         bot.listen do |message|
-          resp = Struct.new(:message, :bot).new(message, bot)
-          event_handler(resp)
+          # resp = Struct.new(:message, :bot).new(message, bot)
+          # event_handler(resp)
+          case message
+          when Telegram::Bot::Types::CallbackQuery
+            # Here you can handle your callbacks from inline buttons
+            if message.data == 'touch'
+              bot.api.send_message(chat_id: message.from.id, text: "Don't touch me!")
+            end
+          when Telegram::Bot::Types::Message
+            kb = [
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Go to Google', url: 'https://google.com'),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Touch me', callback_data: 'touch'),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Switch to inline', switch_inline_query: 'some text')
+            ]
+            markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+            bot.api.send_message(chat_id: message.chat.id, text: 'Make a choice', reply_markup: markup)
+          end
         end
       end
     end
